384594788e5428a06b161b55c3f1a82196be0ef4
Eran Grinberg <soniceran@gmail.com>
2024-01-20 00:30:20 +0100

    BUGFIX | Fix variable names and formatting in vue-web-component-wrapper.es.js

--- package/dist/vue-web-component-wrapper.es.js
+++ package/dist/vue-web-component-wrapper.es.js
@@ -14,65 +14,65 @@ function y(e) {
 const S = ({
   rootComponent: e,
   plugins: i,
-  cssFrameworkStyles: m,
-  VueDefineCustomElement: l,
-  h: s,
+  cssFrameworkStyles: l,
+  VueDefineCustomElement: u,
+  h: o,
   createApp: a,
   getCurrentInstance: c,
   elementName: p,
-  disableRemoveStylesOnUnmount: u
-}) => l({
-  styles: [m],
+  disableRemoveStylesOnUnmount: m
+}) => u({
+  styles: [l],
   props: {
     ...e.props,
     modelValue: { type: [String, Number, Boolean, Array, Object] }
     // v-model support
   },
-  emits: e.emits,
+  emits: e == null ? void 0 : e.emits,
   setup(w) {
     var v;
-    const _ = [...e.emits, "update:modelValue"], n = a();
+    const _ = [...(e == null ? void 0 : e.emits) || [], "update:modelValue"], n = a();
     n.component("app-root", e), n.mixin({
       mounted() {
-        var o;
-        const t = (r) => {
-          r != null && r.length && (this.__style = document.createElement("style"), this.__style.innerText = r.join().replace(/\n/g, ""), b(this.$el).prepend(this.__style));
+        var r;
+        const t = (s) => {
+          s != null && s.length && (this.__style = document.createElement("style"), this.__style.innerText = s.join().replace(/\n/g, ""), b(this.$el).prepend(this.__style));
         };
-        if (t((o = this.$) == null ? void 0 : o.type.styles), this.$options.components)
-          for (const r of Object.values(this.$options.components))
-            t(r.styles);
+        if (t((r = this.$) == null ? void 0 : r.type.styles), this.$options.components)
+          for (const s of Object.values(this.$options.components))
+            t(s.styles);
       },
       unmounted() {
         var t;
-        u || (t = this.__style) == null || t.remove();
+        m || (t = this.__style) == null || t.remove();
       }
     }), n.use(i);
     const d = c();
     if (Object.assign(d.appContext, n._context), Object.assign(d.provides, n._context.provides), process.env.NODE_ENV === "development" && window.__VUE_DEVTOOLS_GLOBAL_HOOK__) {
       const t = document.querySelector(p);
       n._container = t, n._instance = d;
-      const o = {
+      const r = {
         Comment: Symbol("v-cmt"),
         Fragment: Symbol("v-fgt"),
         Static: Symbol("v-stc"),
         Text: Symbol("v-txt")
       };
-      window.__VUE_DEVTOOLS_GLOBAL_HOOK__.emit("app:init", n, n.version, o), window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = n;
+      window.__VUE_DEVTOOLS_GLOBAL_HOOK__.emit("app:init", n, n.version, r), window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = n;
     }
-    const O = _ == null ? void 0 : _.reduce((t, o) => {
-      const r = y(o);
-      return t[r] = (E) => {
-        d.emit(o, E);
+    const O = _ == null ? void 0 : _.reduce((t, r) => {
+      const s = y(r);
+      return t[s] = (E) => {
+        d.emit(r, E);
       }, t;
-    }, {}), f = (v = e == null ? void 0 : e.namedSlots) == null ? void 0 : v.reduce((t, o) => (t[o] = () => s("slot", { name: o }), t), {});
-    return () => s(
+    }, {}), f = (v = e == null ? void 0 : e.namedSlots) == null ? void 0 : v.reduce((t, r) => (t[r] = () => o("slot", { name: r }), t), {});
+    return () => o(
       e,
       {
         ...w,
         ...O
       },
       {
-        default: () => s("slot"),
+        default: () => o("slot"),
         ...f
       }
     );
@@ -80,13 +80,13 @@ const S = ({
 }), h = ({
   elementName: e,
   rootComponent: i,
-  plugins: m,
-  cssFrameworkStyles: l,
-  VueDefineCustomElement: s,
+  plugins: l,
+  cssFrameworkStyles: u,
+  VueDefineCustomElement: o,
   h: a,
   createApp: c,
   getCurrentInstance: p,
-  disableRemoveStylesOnUnmount: u
+  disableRemoveStylesOnUnmount: m
 }) => {
   if (!i) {
     console.warn("No root component provided. Please provide a root component to create a web component.");
@@ -96,7 +96,7 @@ const S = ({
     console.warn("No element name provided. Please provide an element name to create a web component.");
     return;
   }
-  if (!s) {
+  if (!o) {
     console.warn(
       "No VueDefineCustomElement provided. Please provide a VueDefineCustomElement to create a web component."
     );
@@ -118,14 +118,14 @@ const S = ({
     e,
     S({
       rootComponent: i,
-      plugins: m,
-      cssFrameworkStyles: l,
-      VueDefineCustomElement: s,
+      plugins: l,
+      cssFrameworkStyles: u,
+      VueDefineCustomElement: o,
       h: a,
       createApp: c,
       getCurrentInstance: p,
       elementName: e,
-      disableRemoveStylesOnUnmount: u
+      disableRemoveStylesOnUnmount: m
     })
   );
 };
