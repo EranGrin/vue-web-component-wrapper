387ef71e509e3ccc798fddcfa45ef5d517d0c2bf
Eran Grinberg <soniceran@gmail.com>
2024-01-20 00:29:37 +0100

    BUGFIX | Update index.html and App.vue files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@
 All notable changes to this project will be documented in this file. The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased]
+## [1.3.4] - 19.01.2024
+### Fixed
+- Fixed WEBPACK 5 support example
+- Fixed issue with 'emits' option
 
 ## [1.3.0] - 19.01.2024
 ### Added
--- demo-app-webpack/src/App.vue
+++ demo-app-webpack/src/App.vue
@@ -1,62 +1,107 @@
 <template>
-    <div
-     
-    >
-        <header
-         class="bg-blue-300 rounded-lg shadow-lg p-4 mx-0 md:mx-20"
-        >
+    <div>
+      <header class="bg-blue-300 rounded-lg shadow-lg p-4 mx-0 md:mx-20">
+  
         <nav>
-            <ul class="flex justify-between sm:mx-1 md:mx-20">
-                <li>
-                    <router-link to="/">Home</router-link>
-                </li>
-                <li>
-                    <router-link to="/test1">Test route 1</router-link>
-                </li>
-                <li>
-                    <router-link to="/test2">Test route 2</router-link>
-                </li>
-            </ul>
+          <ul class="flex justify-between sm:mx-1 md:mx-20">
+            <li>
+              <router-link to="/">Home</router-link>
+            </li>
+            <li>
+              <router-link to="/test1">Test route 1</router-link>
+            </li>
+            <li>
+              <router-link to="/test2">Test route 2</router-link>
+            </li>
+
+          </ul>
         </nav>
-        </header>
-        <main
-        class="mt-4 p-4 mx-0 md:mx-20 bg-gray-200 rounded-lg shadow-lg" 
+      </header>
+      <header
+        class="bg-gray-800 text-white text-center p-4 mt-4 rounded-lg shadow-lg mx-0 md:mx-20"
+      >
+      <div 
+         style="display: flex; justify-content: space-between; "
         >
-            <router-view
-            />
-        </main>
+        <div class=" text-black flex">
+          <input
+          class="p-2 rounded-lg shadow-lg"
+          :value="modelValue"
+          @input="$emit('update:modelValue', $event?.target?.value)"
+          />
+          <div class="text-white ml-4 align self-center">
+            {{ modelValue }}
+          </div>
+        </div>
+        
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          @click="testEmit"
+        >
+          emit event outside
+        </button>
+      </div>
+      </header>
+      <main class="mt-4 p-4 mx-0 md:mx-20 bg-gray-200 rounded-lg shadow-lg">
+        <router-view />
+      </main>
+      <footer class="bg-gray-800 text-white text-center p-4 mt-4 rounded-lg shadow-lg mx-0 md:mx-20">
+        <div class="mb-2">
+  
+          <slot></slot>
+        </div>
+        <div class="flex justify-center">
+          <slot name="customName"></slot>
+        </div>
+      </footer>
     </div>
-</template>
-
-<script>
-
-export default {
+  </template>
+  
+  <script>
+  export default {
     name: 'App',
+    namedSlots: ['customName'],
+    emits: ['customEventTest', 'update:modelValue'],
     props: {
-        apiToken: { type: String, required: true },
-        baseUri: { type: String, default: 'test.me' },
+      apiToken: { type: String, required: true },
+      baseUri: { type: String, default: 'test.me' },
+      modelValue: { type: String },
     },
-
+  
     data: () => ({
-        configuration: {},
-        credentials: {
-            apiToken: '',
-            baseUri: '',
-        },
+      configuration: {},
+      credentials: {
+        apiToken: '',
+        baseUri: '',
+      },
     }),
-
-    created() {
-        console.log(this);
+  
+  
+    methods: {
+      testEmit() {
+        this.$emit('customEventTest', 
+          { 
+            testEvent: '123456789',
+          }
+        )
+      }
     },
-
-};
-</script>
-<style >
-.test-heading {
+  }
+  </script>
+  <style>
+  .test-heading {
     font-size: 2em;
     margin-top: 2rem;
-}
-a {
-  @apply text-gray-900 hover:text-gray-700;
-}
-</style>
+  }
+  a {
+    @apply text-gray-900 hover:text-gray-700;
+  }
+  
+  header  {
+    @apply font-sans;
+  }
+  
+  main {
+    @apply font-sans;
+  }
+  </style>
