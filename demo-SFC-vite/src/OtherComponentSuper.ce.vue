18aa4e0b43e6f755641a6acea1b7df119eeb316d
OllyNicholass <OllyNicholass@users.noreply.github.com>
2024-07-08 14:51:49 +0100

    chore(#26): update SFC demo to show css order

---
+++ demo-SFC-vite/src/OtherComponentSuper.ce.vue
@@ -0,0 +1,22 @@
+<template>
+  <OtherComponent>
+    <div><slot></slot></div>
+  </OtherComponent>
+</template>
+  
+<script lang="ts">
+import OtherComponent from './OtherComponent.ce.vue'
+  export default {
+    name: 'OtherComponentSuper',
+    components: {
+      OtherComponent
+    }
+  }
+</script>
+  
+<style>
+  div {
+    color: green;
+  }
+</style>
+  
