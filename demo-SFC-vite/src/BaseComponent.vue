5212f3b946364d47a5d6b974ad9a4485b0cef42b
Eran Grinberg <soniceran@gmail.com>
2024-07-12 12:09:14 +0200

    chore: add example of base component to the sfc demo

---
+++ demo-SFC-vite/src/BaseComponent.vue
@@ -0,0 +1,16 @@
+<template>
+      <div id="base-test">Base Component</div>
+  </template>
+    
+  <script lang="ts">
+    export default {
+      name: 'BaseComponent',
+    }
+  </script>
+    
+  <style>
+    #base-test {
+      color: rgb(251, 0, 255);
+    }
+  </style>
+    
--- demo-SFC-vite/src/MyComponent.ce.vue
+++ demo-SFC-vite/src/MyComponent.ce.vue
@@ -1,10 +1,13 @@
 <template>
+  <BaseComponent />
   <div id="test">Hello, Web Component!</div>
   <slot></slot>
 </template>
 
 <script lang="ts">
+import BaseComponent from './BaseComponent.vue';
 export default {
+  components: { BaseComponent },
   name: 'MyComponent',
 }
 </script>
